53aafc52846a5115b3a0285965ce55d08a726722
melih.ilter <melih.ilter@renault.com>
2019-07-24 13:30:00 +0300

    initial commit

---
+++ ORLogger/EventViewerLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ORLogger
+{
+    public class EventViewerLogger : ILogger
+    {
+        private EventLog _eventLog;
+        private string _entryMessage;
+        private string _source;
+        private EventLogEntryType _eventLogEntryType;
+        public EventViewerLogger(string source, string logName)
+        {
+            _source = source;
+            _eventLog = new EventLog(logName);
+            _eventLogEntryType = EventLogEntryType.Error;
+        }
+
+        public EventViewerLogger(string source, string logName, EventLogEntryType eventLogEntryType)
+        {
+            _source = source;
+            _eventLog = new EventLog(logName);
+            _eventLogEntryType = eventLogEntryType;
+        }
+
+
+        public void Log(Exception e)
+        {
+            _entryMessage = string.Format("{0}: {1}", e.Message, e.StackTrace);
+            _eventLog.Source = _source;
+            _eventLog.WriteEntry(_entryMessage, _eventLogEntryType, 101, 1);
+        }
+    }
+}
---
+++ ORLogger/FileLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORLogger
+{
+    public class FileLogger : ILogger
+    {
+        private string _baseDirectory;
+        private string _fileName;
+        private string _content;
+
+        public FileLogger()
+        {
+            _baseDirectory = AppDomain.CurrentDomain.BaseDirectory + "error\\";
+            _fileName = string.Empty;
+            _content = string.Empty;
+        }
+
+        public void Log(Exception e)
+        {
+            _fileName = string.Format("Error_{0}.log", Guid.NewGuid().ToString().Replace("-", "").ToUpper());
+            if (!Directory.Exists(_baseDirectory))
+                Directory.CreateDirectory(_baseDirectory);
+
+            _content += string.Format("{0}{1}", DateTime.Now, Environment.NewLine);
+            _content += string.Format("Exception: {0}{1}", e.Message, Environment.NewLine);
+            _content += string.Format("Stack Trace: {0}{1}", e.StackTrace, Environment.NewLine);
+
+            File.WriteAllText(_baseDirectory + _fileName, _content);
+        }
+    }
+}
